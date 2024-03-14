afda885c1028f2cebbf21ce14ac14147a8b437bc
Mateus Bertazzo <mateusbataglin52@gmail.com>
2024-03-14 15:09:42 -0300

    cria fila no rabbitMQ e cria também EmailConsumer

---
+++ src/main/java/com/ms/email/consumers/EmailConsumer.java
@@ -0,0 +1,13 @@
+package com.ms.email.consumers;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailConsumer {
+    @RabbitListener(queues = "${broker.queue.email.name}")
+    public void listenEmailQueue(@Payload String email) {
+        System.out.println(email);
+    }
+}
